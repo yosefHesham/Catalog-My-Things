d771c36efdcc74813c704912a5c82a12fbce0548
Vanel <vanelnwaba@gmail.com>
2023-02-17 15:39:20 +0100

    Add message when games or authors are empty

--- authors/author_manager.rb
+++ authors/author_manager.rb
@@ -1,9 +1,21 @@
+require_relative './author'
+
 class AuthorsManager
   def initialize
     @authors = []
+    authors = [Author.new('John', 'Doe'), Author.new('Jane', 'Doe')]
+    authors.each { |author| @authors.push(author) }
   end
 
   def list_all_authors
+    puts ''
+    if @authors.empty?
+      puts 'No authors recorded yet.'
+      return
+    end
+    puts 'Authors List :'
+    puts ''
     @authors.each { |author| puts "ID: #{author.id} FirstName: #{author.fisrt_name}, LastName: #{author.last_name}" }
+    puts ''
   end
 end
